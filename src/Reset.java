05bdaa69b396eae82ad79e19558086507adbd8eb
Mehadi Hossain <mehadi_hossain@yahoo.com>
2017-05-02 20:06:00 +0600

    Initial commit

---
+++ src/Reset.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AashiQ
+ */
+public class Reset {
+    void reset()
+    {
+        Board k=new Board();
+            k.setVisible(true);
+    }
+    
+}
